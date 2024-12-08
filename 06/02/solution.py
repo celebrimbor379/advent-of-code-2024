961c9164121728a8305368d394aba51bd44aecbd
radagast <you@example.com>
2024-12-08 05:47:30 -0500

    Day 6, part 2 - eff this stuff, trying to test each potential obstacle during intial traversal has too many corner cases. Going to try a brute-force approach.

--- 06/02/solution.py
+++ 06/02/solution.py
@@ -51,8 +51,10 @@ def check_for_loop(grid, position, seen, direction):
     
     while position.on_grid(grid):
         # print(position)
-        if position.on_grid(grid) and (position in seen) and (direction in seen.get(position)):
-            # pp('Found loop at {} in direction {} for condition {} and seen {}'.format(position, direction, (direction in seen.get(tuple(position))), seen.get(tuple(position))))
+        if (position.on_grid(grid)
+            and (position in seen)
+            and (direction in seen.get(position))):
+        # pp('Found loop at {} in direction {} for condition {} and seen {}'.format(position, direction, (direction in seen.get(tuple(position))), seen.get(tuple(position))))
             return 1
 
         seen.setdefault(position, set()).add(direction)
@@ -82,9 +84,9 @@ def get_solution(input_path):
                 row[row.index('^')] = '.'
             grid.append(row)
 
-    print()
+    # print()
     # print(position)
-    # pp(grid)
+    # print(grid)
 
     while position.on_grid(grid):
         seen.setdefault(position, set()).add(direction)
@@ -97,10 +99,17 @@ def get_solution(input_path):
         else:
             branch_dir = Direction.turn(direction)
             branch_pos = position.inc(branch_dir)
-            if branch_pos.on_grid(grid) and grid[branch_pos.row][branch_pos.col] != '#':
-                sims += 1
+            if (next_pos.on_grid(grid)
+                and next_pos not in seen
+                and branch_pos.on_grid(grid)
+                and grid[branch_pos.row][branch_pos.col] != '#'):
+                # sims += 1
+                # print('sims={}'.format(sims))
                 # pp('Seen before check {}'.format(seen))
+                grid[next_pos.row][next_pos.col] = '#'
                 x = check_for_loop(grid, branch_pos, copy.deepcopy(seen), branch_dir)
+                grid[next_pos.row][next_pos.col] = '.'
+                # x = check_for_loop(grid, branch_pos, seen, branch_dir)
                 result += x
                 # if x > 0:
                     # print('Result incremented to {}'.format(result))
@@ -109,7 +118,7 @@ def get_solution(input_path):
     
     # for r in grid:
         # print(r)
-    print('Ran {} sims'.format(sims))
+    # print('Ran {} sims'.format(sims))
     return result
 
 if __name__ == "__main__":
